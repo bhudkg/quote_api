0bcc7562ff9335d4f31c7a0908f598435d8a9f36
bhudkg <42385281+bhudkg@users.noreply.github.com>
2024-06-07 21:16:42 +0530

    adding final code

--- quotesApi/quoteapp/models.py
+++ quotesApi/quoteapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# class QuoteModel(models.Model):
+#     id = models.AutoField(unique=True, primary_key=True)
+#     category = models.CharField(max_length=200)
+#     author = models.CharField(max_length=50)
+#     quote = models.TextField(max_length=200)
+
+#     def __str__(self) -> str:
+#         return str(self.id)
+    
+
--- quotesApi/quoteapp/views.py
+++ quotesApi/quoteapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+import requests
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    api_url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-05-07&sortBy=publishedAt&apiKey=3e4beeca7c1b41a298498490f8dded20'
+    response = requests.get(api_url)
+    data = response.json()
+
+    context = {
+        'data':data
+    }
+
+    return render(request, 'index.html', context)
+
